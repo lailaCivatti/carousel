017949d8bcfefd8a9959094b5ac141b9c14d8f80
Laila MC <laila.civatti@gmail.com>
2022-08-19 18:41:25 +0200

    navigation updated; changed position of arrow btns

--- main.js
+++ main.js
@@ -1,21 +1,76 @@
 // Variables
-const carousel = document.querySelector(".carousel");
+
 const slides = document.querySelectorAll(".slide");
-const navDots = document.querySelectorAll(".dots");
+const navDots = document.querySelectorAll(".dot");
+const navDotStart = navDots.item(0);
+const navDotPrev = navDots.item(1);
+const navDotNext = navDots.item(2);
+const navDotEnd = navDots.item(3);
 const nextBtn = document.querySelector(".btn-next");
 const prevBtn = document.querySelector(".btn-prev");
-let lastSlide = slides.item(slides.length - 1);
+const leftSlide = slides.item(slides.length - 1);
+const centerSlide = slides.item(0);
+const rightSlide = slides.item(1);
+const navNum = document.querySelector(".navigation-num");
+let pagNum;
+// Functions
 
-// Slider || Carousel
+// Pagination
 
-// change slides
+function getCenterSlide() {
+    for (let i = 0; i <= slides.length - 1; i++) {
+        let slide = slides.item(i);
+        if (slide.getAttribute("class").includes("slide--center")) {
+            pagNum = i + 1;
+        }
+    }
+    navNum.textContent = `${pagNum}`;
+};
+
+    // Navigation dots
+
+// resets to first slide
+function start() {
+    
+    for (const slide of slides) {
+        slide.removeAttribute("class");
+    };
+    leftSlide.setAttribute("class", "slide slide--left");
+    centerSlide.setAttribute("class", "slide slide--center");
+    rightSlide.setAttribute("class", "slide slide--right");
+    for (const slide of slides) {
+        if (!slide.getAttribute("class")) {
+        slide.setAttribute("class", "slide slide--out");
+        }
+    };
+    getCenterSlide();
+};
+
+// resets to last slide
+function end() {
+    
+    for (const slide of slides) {
+        slide.removeAttribute("class");
+    };
+    leftSlide.setAttribute("class", "slide slide--center");
+    centerSlide.setAttribute("class", "slide slide--right");
+    slides.item(slides.length - 2).setAttribute("class", "slide slide--left");
+    for (const slide of slides) {
+        if (!slide.getAttribute("class")) {
+        slide.setAttribute("class", "slide slide--out");
+        }
+    };
+    getCenterSlide();
+};
+
+    // Arrow Buttons
 
 function nextSlide() {
 
     // place last slide of array out of layout before starting the loop for the first time after loading page, because it is to the left in the beginning
-    if (lastSlide.getAttribute("class").includes("slide--left")) {
-        lastSlide.removeAttribute("class", "slide--left");
-        lastSlide.setAttribute("class","slide--out");
+    if (leftSlide.getAttribute("class").includes("slide--left")) {
+        leftSlide.removeAttribute("class", "slide--left");
+        leftSlide.setAttribute("class","slide--out");
     }
 
     for (let i = 0; i <= slides.length - 1; i++) {
@@ -43,7 +98,8 @@ function nextSlide() {
                 }
             }
         }
-    }
+    };
+    getCenterSlide();
 };
 
 function prevSlide() {
@@ -73,13 +129,20 @@ function prevSlide() {
                 }
             }
         }
-    }
+    };
+    getCenterSlide();
 };
 
+// Events
 
-// Navigation Buttons
+    // Arrow Buttons
 
 nextBtn.addEventListener("click", nextSlide);
 prevBtn.addEventListener("click", prevSlide);
 
-// Navigation Dots
+    // Navigation Dots
+
+navDotStart.addEventListener("click", start);
+navDotPrev.addEventListener("click", prevSlide);
+navDotNext.addEventListener("click", nextSlide);
+navDotEnd.addEventListener("click", end);
