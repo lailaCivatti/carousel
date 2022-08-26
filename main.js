6ea1d6fb46415f1507366e87e640b47a840d0b2e
Laila MC <laila.civatti@gmail.com>
2022-08-26 14:41:17 +0200

    transition .slide; set/remove att. for classList

--- main.js
+++ main.js
@@ -72,32 +72,32 @@ function nextSlide() {
 
     // place last slide of array out of layout before starting the loop for the first time after loading page, because it is to the left in the beginning
     if (leftSlide.getAttribute("class").includes("slide--left")) {
-        leftSlide.removeAttribute("class", "slide--left");
-        leftSlide.setAttribute("class","slide--out");
+        leftSlide.classList.remove("slide--left");
+        leftSlide.classList.add("slide--out");
     }
 
     for (let i = 0; i <= slides.length - 1; i++) {
         let slide = slides.item(i);
 
         if (slide.getAttribute("class").includes("slide--left")) {
-            slide.removeAttribute("class", "slide--left");
-            slide.setAttribute("class","slide--out");
+            slide.classList.remove("slide--left");
+            slide.classList.add("slide--out");
         } else if (slide.getAttribute("class").includes("slide--center")) {
-            slide.removeAttribute("class", "slide--center");
-            slide.setAttribute("class","slide--left");
+            slide.classList.remove("slide--center");
+            slide.classList.add("slide--left");
         } else if (slide.getAttribute("class").includes("slide--right")) {
-            slide.removeAttribute("class", "slide--right");
-            slide.setAttribute("class","slide--center");
+            slide.classList.remove("slide--right");
+            slide.classList.add("slide--center");
             // if the last slide to the right is reached, grab the first slide from array to make a full circle
             if (i === slides.length-1) {
-                slides.item(0).setAttribute("class", "slide--right");
+                slides.item(0).classList.add("slide--right");
             }
         } else if (slide.getAttribute("class").includes("slide--out")) {
             // if the slide is out and the previous slide has just become the center slide, then put slide on the right side
             if (slide.previousElementSibling) {
                 if (slide.previousElementSibling.getAttribute("class").includes("slide--center")) {
-                slide.removeAttribute("class", "slide--out");
-                slide.setAttribute("class","slide--right");
+                slide.classList.remove("slide--out");
+                slide.classList.add("slide--right");
                 }
             }
         }
@@ -111,24 +111,24 @@ function prevSlide() {
         let slide = slides.item(i);
 
         if (slide.getAttribute("class").includes("slide--left")) {
-            slide.removeAttribute("class", "slide--left");
-            slide.setAttribute("class","slide--center");
+            slide.classList.remove("slide--left");
+            slide.classList.add("slide--center");
             // if the last slide to the left is reached, grab the first slide from array to make a full circle
             if (i == 0) {
-                slides.item(slides.length-1).setAttribute("class", "slide--left");
+                slides.item(slides.length-1).classList.add("slide--left");
             }
         } else if (slide.getAttribute("class").includes("slide--center")) {
-            slide.removeAttribute("class", "slide--center");
-            slide.setAttribute("class","slide--right");
+            slide.classList.remove("slide--center");
+            slide.classList.add("slide--right");
         } else if (slide.getAttribute("class").includes("slide--right")) {
-            slide.removeAttribute("class", "slide--right");
-            slide.setAttribute("class","slide--out");
+            slide.classList.remove("slide--right");
+            slide.classList.add("slide--out");
         } else if (slide.getAttribute("class").includes("slide--out")) {
             // if the slide is out and the next slide has just become the center slide, then put slide on the left side
             if (slide.nextElementSibling) {
                 if (slide.nextElementSibling.getAttribute("class").includes("slide--center")) {
-                slide.removeAttribute("class", "slide--out");
-                slide.setAttribute("class","slide--left");
+                slide.classList.remove("slide--out");
+                slide.classList.add("slide--left");
                 }
             }
         }
